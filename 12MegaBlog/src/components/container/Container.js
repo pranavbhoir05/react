55972293048bfbe07fe5360358ed091ea75e3b15
Pranav Bhoir <pranavbhoir182000@gmail.com>
2026-03-11 23:18:48 +0530

    feat: add Container component for layout structure

---
+++ 12MegaBlog/src/components/container/Container.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Container({nameanything}) {
+  return <div className='w-full max-w-7 mx-auto px-4'>
+        {nameanything}</div> ;
+  
+}
+
+export default Container
