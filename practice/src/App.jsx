cabc47f53975e3afff53b9e36213c50fcf939558
Pranav Bhoir <pranavbhoir182000@gmail.com>
2026-05-14 17:35:42 +0530

    feat: refactor state management in App component to use object for count and update click handler

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 
 
 function App() {
-  const [value, setValue] = useState(1)
+  console.log('app render',Math.random());
+  
+  const [value, setValue] = useState({
+    count: 1 
+  })
   // const [multipliedValue, setMultipliedValue] = useState(5)
 
   let multipliedValue = value * 5
 
- const multiplybyfive = () =>{
+ const clickMe = () =>{ 
   // setMultipliedValue(value * 5) 
-   setValue((prev)=>prev + 1 )
+      setValue({
+        count: 1
+      })
+     // there is hidden feature in setValue which is (prev) => (prev) which gives us the previous value of the state and we can use it to update the state based on the previous value 
+    // setValue((prev) => (prev < 10 ? prev + 1 : prev))
+    console.log(`count ${value.count}`);
+    
  }
+ 
+//  useEffect(()=> {},[value.count])
 
   
  //no need of useEffect as we can directly calculate the multiplied value in the render method and it will re-render whenever the value changes
@@ -22,8 +34,8 @@ function App() {
   return (
     <>
     <div className="bg-gray-500 w-full h-screen flex flex-col items-center justify-center">
-    <h1 className='text-green-500'>Current number: {value}</h1>
-    <button className='bg-blue-500 text-white px-4 py-2 rounded'onClick={multiplybyfive} >click to multiply by 5</button>
+    <h1 className='text-green-500'>Current number: {value.count}</h1>
+    <button className='bg-blue-500 text-white px-4 py-2 rounded' disabled={value >= 10} onClick={clickMe} >click to multiply by 5</button>
     <h2 className='text-white'>Multiplied value : {multipliedValue}</h2>
     </div>
     </>
